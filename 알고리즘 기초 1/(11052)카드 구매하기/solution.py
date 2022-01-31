a9c6729586bf290b0f9f8079d6c6369f387acd50
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-01-31 12:53:34 +0900

    Update solution.py

--- 알고리즘 기초 1/(11052)카드 구매하기/solution.py
+++ 알고리즘 기초 1/(11052)카드 구매하기/solution.py
@@ -7,6 +7,14 @@ res = 0
 
 for i in range(num):
     cost.append(int(cost_char[i])/(i+1))
+    
+while num != 0:
+    temp_list = cost[:num]
+    max_num = max(temp_list)
+    max_index = temp_list.index(max_num)
+    res += max_num * (max_index+1)
+    num -= max_index+1
+    
+res = int(res)
 
-
-print(cost)
+print(res)
