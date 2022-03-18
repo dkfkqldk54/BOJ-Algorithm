7a1a1ddbf1f1ffc976e6b30f4c60a00a85ce1152
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-03-18 21:40:57 +0900

    Update solution.py

--- 알고리즘 기초 1/(1309)동물원/solution.py
+++ 알고리즘 기초 1/(1309)동물원/solution.py
@@ -1,25 +1,11 @@
-turn = int(input())
-
-dp = []
-dp.append([0, 0, 0, 0])
-dp.append([3, 0, 0, 0])
-dp.append([5, 2, 0, 0])
-dp.append([7, 8, 2, 0])
-dp.append([9, 18, 12, 2])
-
-if turn >= 5:
-  for i in range(turn-4):
-    dp.append([0, 0, 0, 0])
-  for i in range(5, turn+1):
-    dp[i][0] = 2 * i + 1
-    dp[i][0] %= 9901
-    dp[i][1] = 4 * i - 6 + dp[i-1][1]
-    dp[i][1] %= 9901
-    dp[i][2] = 2 * dp[i-1][1] - 4 * i + 10 + dp[i-1][2]
-    dp[i][2] %= 9901
-    dp[i][3] = dp[i-1][2] + dp[i-1][3]
-    dp[i][3] %= 9901
-
-res = sum(dp[turn])
+num = int(input())
+dp = [[0] * 3 for i in range(num+1)]
+for i in range(3):
+    dp[1][i] = 1
+for i in range(2, num+1):
+    dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % 9901
+    dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901
+    dp[i][2] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 9901
+res = sum(dp[num])
 res %= 9901
 print(res)
