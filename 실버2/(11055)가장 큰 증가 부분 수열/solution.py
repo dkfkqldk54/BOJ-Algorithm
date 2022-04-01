1cb1b59097313f81df73c283cba3d2c729d0d0de
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-04-01 18:30:21 +0900

    Create solution.py

---
+++ 실버2/(11055)가장 큰 증가 부분 수열/solution.py
@@ -0,0 +1,10 @@
+turn = int(input())
+num_list = list(map(int, input().split()))
+dp = [i for i in num_list]
+
+for i in range(1, turn):
+  for j in range(i):
+    if num_list[i] > num_list[j]:
+      dp[i] = max(dp[i], dp[j]+num_list[i])
+
+print(max(dp))
