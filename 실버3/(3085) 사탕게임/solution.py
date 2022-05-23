4f2edc6b63f901a8b9777f49a77a7d1ce356fa11
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-05-23 09:33:07 +0900

    Create solution.py

---
+++ 실버3/(3085) 사탕게임/solution.py
@@ -0,0 +1,43 @@
+n = int(input())
+arr = [list(input()) for i in range(n)]
+max_candle = 0
+
+def width_max():
+  global max_candle
+  for i in range(n):
+    for j in range(n-1):
+      stack = 0
+      if arr[i][j] == arr[i][j+1]:
+        stack += 1
+        max_candle = max(max_candle, stack)
+      else:
+        stack = 0
+
+def height_max():
+  global max_candle
+  for i in range(n):
+    for j in range(n-1):
+      stack = 0
+      if arr[j][i] == arr[j+1][i]:
+        stack += 1
+        max_candle = max(max_candle, stack)
+      else:
+        stack = 0
+
+for i in range(n):
+  for j in range(n-1):
+    if arr[i][j] != arr[i][j+1]:
+      arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
+      width_max()
+      height_max()
+      arr[i][j+1], arr[i][j] = arr[i][j], arr[i][j+1]
+
+    if arr[j][i] != arr[j+1][i]:
+      arr[j][i], arr[j+1][i] = arr[j+1][i], arr[j][i]
+      width_max()
+      height_max()
+      arr[j+1][i], arr[j][i] = arr[j][i], arr[j+1][i]
+
+max_candle += 1
+
+print(max_candle)
