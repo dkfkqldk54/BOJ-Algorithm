09bb74830687157a32d67c9085b7ee8aa8feaa7a
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-05-25 11:10:55 +0900

    Update solution.py

--- 골드5/(1107) 리모컨/solution.py
+++ 골드5/(1107) 리모컨/solution.py
@@ -1,36 +1,60 @@
-channel = [str(i) for i in range(501)]
+channel = [str(i) for i in range(500001)]
 target = input()
 n = int(input())
-ban = list(input().split())
 
-for i in range(n):
-  for j in range(501):
-    if channel[j].find(ban[i]) >= 0:
-      channel[j] = '0'
+if n == 0:
+  if target == '100':
+    print(0)
+  else:
+    print(len(target))
 
-if target == '100':
-  print(0)
-elif target in ban:
-  print(len(target))
 else:
-  target = int(target)
-  i = 0
-  j = 0
-  up = 999999
-  down = 999999
+  ban = list(input().split())
 
-  channel[100] = '100'
-  
-  while target + i < 501:
-    i += 1
-    if channel[target + i] != '0':
-      up = i
-      break
+  if target == '100':
+    print(0)
+  else:
+    for i in range(n):
+      for j in range(500001):
+        if channel[j].find(ban[i]) >= 0:
+          channel[j] = 'X'
 
-  while target - j > 0:
-    j += 1
-    if channel[target-j] != '0':
-      down = j
-      break
+    if target in channel:
+      print(len(target))
 
-  res = len
+    else:
+      channel[100] = '100'
+
+      target = int(target)
+      up_target = target
+      down_target = target
+      up = 0
+      down = 0
+
+      while up_target < 500000:
+        up_target += 1
+        if channel[up_target] != 'X':
+          up = up_target - target
+          break
+
+      while down_target >= 0:
+        down_target -= 1
+        if channel[down_target] != 'X':
+          down = target - down_target
+          break
+    
+      if up == 0 and down != 0:
+        res = len(channel[down_target])
+        res += down
+      elif down == 0 and up != 0:
+        res = len(channel[up_target])
+        res += up
+      else:
+        if up >= down:
+          res = len(channel[down_target])
+          res += down
+        else:
+          res = len(channel[up_target])
+          res += up
+
+      print(res)
