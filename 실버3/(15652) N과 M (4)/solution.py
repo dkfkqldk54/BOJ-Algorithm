29d3b3501ddcce61206da05653a96d545c809c1a
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-06-23 21:41:26 +0900

    Create solution.py

---
+++ 실버3/(15652) N과 M (4)/solution.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+res = []
+
+def rec(start, depth):
+  if depth == m:
+    print(' '.join(map(str, res)))
+    return
+
+  for i in range(start, n+1):
+      res.append(i)
+      rec(i, depth+1)
+      res.pop()
+      
+rec(1, 0)
