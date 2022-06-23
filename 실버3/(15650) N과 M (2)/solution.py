053bd4f455e7ba2c0d06aa17feca10af83e97a0d
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-06-23 20:24:19 +0900

    Update solution.py

--- 실버3/(15650) N과 M (2)/solution.py
+++ 실버3/(15650) N과 M (2)/solution.py
@@ -1,7 +1,5 @@
 n, m = map(int, input().split())
-
 res = []
-visited = [False for _ in range(n+1)]
 
 def rec(start, depth):
   if depth == m:
@@ -9,11 +7,8 @@ def rec(start, depth):
     return
 
   for i in range(start, n+1):
-    if visited[i] == False:
-      visited[i] = True
       res.append(i)
       rec(i+1, depth+1)
-      visited[i] = False
       res.pop()
-
+      
 rec(1, 0)
